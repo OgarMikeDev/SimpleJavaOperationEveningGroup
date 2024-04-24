7192e826472ee8be31f0d363c868ab94538d747b
PilotVelosipeda <vsotnikov35@mail.ru>
2024-04-24 21:11:19 +0300

    Merge remote-tracking branch 'origin/main'

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,8 +16,6 @@ public class Main {
          */
         String scanner = new Scanner(System.in).nextLine();
         System.out.println(lettersAndNumbers(scanner));
-
-
         /*
         Андрей.
         Написать метод для
