02bdb2c2b386a8691fe0f0b2f2cdd5139867c3a2
Ulianna-778 <ulianna788@mail.ru>
2024-04-24 21:27:36 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -154,6 +154,7 @@ public class Main {
     //Ульяна
     public static void squarerectangle(int a, int b) {
         int P = (a + b) *  2;
+
         System.out.println("Площадь прямоугольника:  " + P);
     }
 
