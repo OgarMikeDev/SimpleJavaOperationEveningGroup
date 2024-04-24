9db5e32fa862257f2dc72233c72e5ea0f220437e
alex21072003 <alexkarpinskiy21@gmail.com>
2024-04-24 19:42:36 +0300

    metod sasha

--- src/main/Main.java
+++ src/main/Main.java
@@ -80,6 +80,7 @@ public class Main {
         Написать метод, вычисляющий площадь круга,
         по заданному в параметре, радиусу.
          */
+        System.out.println(squareCircle(10));
 
         /*
         Михаил
@@ -130,7 +131,7 @@ public class Main {
 
     //Саша
     public static double squareCircle(double radius) {
-        return 0.0;
+        return Math.PI * radius * radius;
     }
 
     //Михаил
